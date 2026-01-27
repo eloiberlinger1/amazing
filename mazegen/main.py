1b89e638576a4ad8e1faeaec884eca03d734f43f
Eloi Berlinger <eloi.berling@protonmail.com>
2026-01-27 13:15:55 +0100

    upgrade maze code structure

--- mazegen/main.py
+++ mazegen/main.py
@@ -2,17 +2,57 @@
 Docstring for mazegen.main
 """
 
+from typing import Tuple, List
+from dataclasses import dataclass
+
 
 class MazeGenerator:
     def __init__(self):
         print("def")
 
 
+@dataclass
 class MazeCell:
     north: bool
     south: bool
     east: bool
     west: bool
+    coord: Tuple[int, int]
+
+
+class MazeManager:
+
+    def __init__(self, height: int, width: int) -> None:
+        self.height = height
+        self.width = width
+        self.elmts = {
+            "h": "─",
+            "v": "│",
+            "c1": "╭",
+            "c2": "╰",
+            "c3": "╮",
+            "c4": "╯",
+            "f": "▒",
+        }
+
+    def get_maze_container(self) -> List[List[MazeCell]]:
+        """Initializes the grid with closed cells."""
+        container = []
+        for i in range(self.height):
+            row = []
+            for j in range(self.width):
+                row.append(
+                    MazeCell(
+                        False,
+                        False,
+                        False,
+                        False,
+                        (i, j),
+                    )
+                )
+            container.append(row)
+
+        return container
 
 
 def print_maze(width: int, height: int) -> None:
@@ -22,20 +62,13 @@ def print_maze(width: int, height: int) -> None:
 
     print("asd")
 
-    walls = {
-        "h": "─",
-        "v": "│",
-        "c1": "╭",
-        "c2": "╰",
-        "c3": "╮",
-        "c4": "╯",
-        "f": "▒",
-    }
+    generator = MazeManager(width, height)
 
-    maze = [[MazeCell() for j in range(width)] for i in range(height)]
-    for j in maze:
-        for i in j:
-            print(walls["v"], end="")
+    maze = generator.get_maze_container()
+
+    for l in maze:
+        for i in l:
+            print(i, end="")
         print()
 
 
